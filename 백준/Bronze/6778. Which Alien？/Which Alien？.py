f5f894711d093847a3dd0b3c730bb5b3b11115d9
PB ho <49295076+forexms78@users.noreply.github.com>
2023-10-28 17:08:18 +0900

    [Bronze IV] Title: Which Alien?, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+a = int(input())
+b = int(input())
+
+if a >= 3 and b <= 4:
+    print('TroyMartian')
+if a <= 6 and b >= 2:
+    print('VladSaturnian')
+if a <= 2 and b <= 3:
+    print('GraemeMercurian')
