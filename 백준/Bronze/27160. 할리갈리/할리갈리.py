896d6b094d7ca73f6a912687f6b351faa2bd7383
PB ho <49295076+forexms78@users.noreply.github.com>
2023-11-03 22:17:44 +0900

    [Bronze II] Title: 할리갈리, Time: 4120 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27160. 할리갈리/할리갈리.py
@@ -0,0 +1,22 @@
+N = int(input())
+
+my_dict = {
+    "STRAWBERRY" : 0,
+    "BANANA" : 0,
+    "LIME" : 0,
+    "PLUM" : 0
+}
+
+for i in range(N):
+    V, A = input().split()
+    my_dict[V] = my_dict[V] + int(A)
+
+X = my_dict["BANANA"]
+Y = my_dict["LIME"]
+Z = my_dict["PLUM"]
+K = my_dict["STRAWBERRY"]
+
+if X == 5 or Y == 5 or Z == 5 or K ==5 :
+    print("YES")
+else:
+    print("NO")
